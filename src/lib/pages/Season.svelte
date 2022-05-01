d13dbb03503bc9ee7d9d910d6927ac5af61ce7e0
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-05-01 02:34:28 +0100

    Season page

---
+++ src/lib/pages/Season.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	const IMAGE_API = 'https://image.tmdb.org/t/p/w500/';
+	export let season_details: Season_type;
+	export let tv_id: string;
+</script>
+
+<section id="season">
+	<div
+		class="max-w-7xl mx-auto text-skin-base xl:mt-5 xl:mb-10 bg-skin-secondary xl:pl-5 pt-1 pb-1 xl:rounded-2xl"
+	>
+		{#if season_details.id}
+			<h4 class="pb-2">Season Information</h4>
+			{#each season_details.episodes as episode}
+				<div class=" bg-skin-primary flex flex-col xl:flex-row m-4 p-2 rounded-lg">
+					<a
+						href={`/episode/${tv_id}/${season_details.season_number}/${episode.episode_number}`}
+						class="flex flex-col xl:flex-row text-skin-base xl:rounded-lg"
+					>
+						<div
+							class="flex mx-auto max-h-full max-w-full xl:m-0 xl:flex-none xl:w-80 xl:h-44 xl:items-start"
+						>
+							<img
+								class="flex xl:w-80 xl:h-44 items-start"
+								src={episode.still_path ? IMAGE_API + episode.still_path : '/default.jpg'}
+								alt="season"
+							/>
+						</div>
+						<div class="ml-2 block">
+							<h4>{episode.name}</h4>
+							<h6>Air Date: {episode.air_date}</h6>
+							<h6>Overview:</h6>
+							<h6 class=" flex-1 pr-8 text-skin-muted mb-4">{episode.overview}</h6>
+						</div>
+					</a>
+				</div>
+			{/each}
+		{/if}
+	</div>
+</section>
---
+++ src/routes/seasons/[id]/[season_number].svelte
@@ -0,0 +1,36 @@
+<script context="module" lang="ts">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ params, fetch }: any) {
+		const res = await (
+			await fetch('/api/postData', {
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				method: 'POST',
+				body: JSON.stringify({
+					api_ref: 'season',
+					id: params.id,
+					season_number: params.season_number
+				})
+			})
+		).json();
+		const season_details = await res.res;
+		return {
+			props: {
+				season_details
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import Season from '$lib/pages/Season.svelte';
+	import { page } from '$app/stores';
+
+	export let season_details: Season_type;
+	let tv_id = $page.params.id;
+</script>
+
+<Season {season_details} {tv_id} />
