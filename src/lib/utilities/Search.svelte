65f3c202460e904bcf3d347665ff0a454597fe62
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-05-01 02:09:40 +0100

    Implement search functionality

--- src/lib/utilities/Search.svelte
+++ src/lib/utilities/Search.svelte
@@ -1,10 +1,29 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let searchTerm: string;
+
+	const handleOnSubmit = () => {
+		if (searchTerm) {
+			let searching = searchTerm;
+			searchTerm = '';
+			goto(`/search/${searching}`);
+		}
+	};
+</script>
+
 <section id="search">
-	<form class="flex justify-end items-center pr-0.5" label="search form">
+	<form
+		class="flex justify-end items-center pr-0.5"
+		label="search form"
+		on:submit|preventDefault={handleOnSubmit}
+	>
 		<input
 			label="search"
 			class="placeholder-gray-500 bg-transparent w-24 xl:w-48 h-7 rounded-full text-l px-2 text-skin-inverted placeholder-text-skin-base::placeholder border-2 border-skin-border"
 			type="text"
 			placeholder="Search..."
+			bind:value={searchTerm}
 		/>
 	</form>
 </section>
---
+++ src/routes/search/[id].svelte
@@ -0,0 +1,43 @@
+<script context="module" lang="ts">
+	import { media_type, data, current_page } from '$lib/stores/store';
+	import { get } from 'svelte/store';
+
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+
+	export async function load({ params, fetch }: any) {
+		current_page.set(1);
+		const res = await (
+			await fetch('/api/postData', {
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				method: 'POST',
+				body: JSON.stringify({
+					api_ref: 'search',
+					media: get(media_type),
+					query: params.id,
+					page: '1'
+				})
+			})
+		).json();
+		data.set(await res.res.results);
+		const total_pages = await res.res.total_pages;
+		return {
+			props: {
+				total_pages
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let total_pages: number;
+	let searching = $page.params.id;
+
+	import MainSection from '$lib/pages/MainSection.svelte';
+	import { page } from '$app/stores';
+</script>
+
+<MainSection {total_pages} {searching} />
