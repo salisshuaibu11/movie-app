624bf23a63c71e92e747157d6a53fe4c310803e4
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-05-01 02:14:35 +0100

    get movies in index file

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,35 @@
+<script context="module" lang="ts">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	import { data, current_page } from '$lib/stores/store';
+	export async function load({ params, fetch }: any) {
+		data.set([]);
+		current_page.set(1);
+		const res = await (
+			await fetch('/api/postData', {
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				method: 'POST',
+				body: JSON.stringify({
+					media: 'movie',
+					api_ref: 'show',
+					page: '1'
+				})
+			})
+		).json();
+		data.set(await res.res.results);
+		const total_pages = await res.res.total_pages;
+
+		return {
+			props: {
+				total_pages
+			}
+		};
+	}
+</script>
+
 <script lang="ts">
 	export let total_pages: number;
 	import MainSection from '$lib/pages/MainSection.svelte';
