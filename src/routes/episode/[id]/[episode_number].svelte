427cf917ede75708c987541bf94bf73c909c3a6f
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-05-01 02:40:07 +0100

    episode page

---
+++ src/routes/episode/[id]/[episode_number].svelte
@@ -0,0 +1,35 @@
+<script context="module" lang="ts">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ params, fetch }: any) {
+		const res = await (
+			await fetch('/api/postData', {
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				method: 'POST',
+				body: JSON.stringify({
+					api_ref: 'episode',
+					id: params.id,
+					season_number: params.season_number,
+					episode_number: params.episode_number
+				})
+			})
+		).json();
+
+		const episode_details = await res.res;
+		return {
+			props: {
+				episode_details
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import Episode from '$lib/pages/Episode.svelte';
+	export let episode_details: Episodes;
+</script>
+
+<Episode {episode_details} />
